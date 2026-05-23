94501faf20188b2b634cb7078e1303ae14a4d456
javiarraiz <javiarraiz@gmail.com>
2026-05-22 19:13:12 -0500

    Plantilla proyecto DRF. Creado aplicación de trabajo, main, dashboard. Aplicación de users, registro y login. agregadas las urls de todas esas vistas pero van a estar comentadas mientras se hace este commit

---
+++ owly/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from owly_app.views import main, dashboard
+
+urlpatterns = [
+    #path('admin/', admin.site.urls),
+    #path('accounts/', include('django.contrib.auth.urls')),
+    #path('', main, name='main'),
+    #path('dashboard/', dashboard, name='dashboard'),
+]
---
+++ owly_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+def main(request):
+    return render(request, 'main.html')
+
+@login_required
+def dashboard(request):
+    return render(request, 'dashboard.html')
